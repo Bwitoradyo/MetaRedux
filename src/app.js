25a7dc3d6c1c21c1728b4026d4f12876438f4e81
Bwitoradyo <bambang.witoradyo@gmail.com>
2017-12-01 17:53:19 -0500

    Immutable State and Pure Function

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@
 import {createStore} from "redux";
 
 //STEP 3 define reducers
-const reducer = (state = {}, action) => {
+const reducer = (state = {books:[]}, action) => {
   switch(action.type){
     case "POST_BOOK":
-    return state = action.payload;
+    return {books:[...state.books, ...action.payload]};
     break;
   }
   return state
@@ -16,16 +16,32 @@ const store = createStore(reducer);
 
 store.subscribe(() => {
   console.log("current state is: ", store.getState());
-  console.log("current price is ", store.getState().price)
 })
 
 //STEP 2 create and dispatch actions
 store.dispatch({
   type:"POST_BOOK",
-  payload: {
+  payload: [{
     id: 1,
     title: "this is the book title",
     description: "this is the book description",
     price: 33.33
-  }
+  },
+  {
+    id: 2,
+    title: "this is the second book title",
+    description: "this is the second book description",
+    price: 50
+  } 
+  ]
 });
+
+store.dispatch({
+  type: "POST_BOOK",
+  payload:[{
+    id:3,
+    title:"this is the third book title",
+    description: "this is the third book description",
+    price: 44
+  }]
+})
