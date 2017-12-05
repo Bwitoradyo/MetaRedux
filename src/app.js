caa5fde662fe987915f04fcdb6af4132c125b66e
bwitoradyo <bambang.witoradyo@gmail.com>
2017-12-04 21:59:35 -0500

    How To Combine Reducers

--- src/app.js
+++ src/app.js
@@ -4,9 +4,49 @@ import {createStore} from "redux";
 //STEP 3 define reducers
 const reducer = (state = {books:[]}, action) => {
   switch(action.type){
+    
     case "POST_BOOK":
-    return {books:[...state.books, ...action.payload]};
-    break;
+      return {books:[...state.books, ...action.payload]};
+      break;
+
+    case "DELETE_BOOK" :
+      //Create a copy of the current array of books
+      const currentBookToDelete = [...state.books]
+      //Determine at which index in books array is the book to be deleted
+      const indexToDelete = currentBookToDelete.findIndex((book) => {
+        return book.id === action.payload.id
+        }
+      )
+      //Use slice to remove the book at the specified index
+      return {books: [...currentBookToDelete.slice(0, indexToDelete),
+      ...currentBookToDelete.slice(indexToDelete + 1)
+      ]}
+      break;
+
+    case "UPDATE_BOOK" :
+      //Create a copy of the current array of books
+      const currentBookToUpdate = [...state.books]
+      //Determine at which index in the books array is the book to be updated
+      const indexToUpdate = currentBookToUpdate.findIndex((book) => {
+        return book.id === action.payload.id
+        }
+      )
+      //Create a new book object with the new values and with the same array index
+      //of the item we want to replace. To achieve this we will use spread operator
+      //although we can also use concat method as well
+      const newBookToUpdate = {
+        ...currentBookToUpdate[indexToUpdate],
+	title: action.payload.title
+      }
+      //This log has the purpose to show you how newBookToUpdate looks like
+      console.log("What is the new book to update?", newBookToUpdate);
+      //Use slice to remove the book at the specified index
+      //replace with the new object and concatenate
+      //with the rest of items in the array
+      return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate,
+      ...currentBookToUpdate.slice(indexToUpdate + 1)
+      ]}
+      break;
   }
   return state
 }
@@ -36,12 +76,17 @@ store.dispatch({
   ]
 });
 
+//DELETE a book
 store.dispatch({
-  type: "POST_BOOK",
-  payload:[{
-    id:3,
-    title:"this is the third book title",
-    description: "this is the third book description",
-    price: 44
-  }]
+  type: "DELETE_BOOK",
+  payload:{id:1}
+})
+
+//UPDATE a book
+store.dispatch({
+  type: "UPDATE_BOOK",
+  payload: {
+    id: 2,
+    title: "Learn Metaredux Now!"
+  }
 })
