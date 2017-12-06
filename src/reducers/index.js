6f25ac009a31c1f4fd2c5f4212889fc9804fc7d6
bwitoradyo <bambang.witoradyo@gmail.com>
2017-12-05 22:03:24 -0500

    Create Action And Reducer For The Cart

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+"use strict";
+import {combineReducers} from "redux";
+
+// IMPORT REDUCERS TO BE COMBINED
+import {booksReducers} from "./booksReducers";
+
+// COMBINE THE REDUCERS
+export default combineReducers({
+  books: booksReducers
+})
